fdf6fd427dcee322ec58a18ceadac5a878fcf8da
Zain Ullah <zainullahachakzai@gmail.com>
2025-07-02 22:05:23 +0500

    tenzies new project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import MainComp from './components/mainComp/mainComp'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <MainComp/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/mainComp/MainComp.jsx
@@ -0,0 +1,7 @@
+export default function MainComp() {
+    return(
+        <main>
+            <h1>Hoho</h1>
+        </main>
+    )
+}
