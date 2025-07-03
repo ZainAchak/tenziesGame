7265e9b252865907b1b441c4f3023aa62ea1f9bd
Zain Ullah <zainullahachakzai@gmail.com>
2025-07-03 19:03:40 +0500

    new updates on Tenzies

--- src/components/Die/Die.jsx
+++ src/components/Die/Die.jsx
@@ -1,9 +1,15 @@
 import styles from "./Die.module.css"
 
 export default function Die(props) {
+    const isHeld = props.dice.isHeld;
+    const value = props.dice.value;
+    const id = props.dice.id;
+    // isHeld ? ()=>{} : 
+
     return(
-        <div className={styles.Die}>
-            <h1>{props.number}</h1>
+        <div onClick={()=>props.setFunction(id)} 
+             className={isHeld ? styles.DieHeld : styles.Die  }>
+             <h1>{value}</h1>
         </div>
     )
 }
--- src/components/mainComp/MainComp.jsx
+++ src/components/mainComp/MainComp.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from "react";
 import Die from "../Die/Die";
 import styles from "./MainComp.module.css"
 
@@ -5,19 +6,85 @@ function generateDiceRolls() {
   const diceRolls = [];
   for (let i = 0; i < 10; i++) {
     const roll = Math.floor(Math.random() * 6) + 1;
-    diceRolls.push(roll);
+    diceRolls.push({id:crypto.randomUUID(),
+                    value:roll, 
+                    isHeld:false});
   }
   return diceRolls;
 }
 
+function getaRandomDice() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+
 export default function MainComp() {
-    const dicesRandomNumber = generateDiceRolls();
+
+    const [dicesRandomNumber,setDicesRandomNumber] = useState(generateDiceRolls());
+    const [newGame, setNewGame] = useState(false);
+    const buttonText = useRef()
+
+    useEffect(()=>{
+        const allHeld = dicesRandomNumber.every(die => die.isHeld === true);
+        if(allHeld){
+            const firstValue = dicesRandomNumber[0]?.value;
+            const allSameValue = dicesRandomNumber.every(die => die.value === firstValue);
+            setNewGame(true);
+
+            if(allSameValue){
+                buttonText.current.textContent = "New Game"
+            }else{
+                buttonText.current.textContent = "Not All Same Selected New Game"
+            }
+        }
+    },[dicesRandomNumber])
+
+    function changeOnHeld(id) {
+        setDicesRandomNumber(prev => 
+            prev.map((item)=>{
+                if (item.id === id) {
+                    return{...item, isHeld : !item.isHeld}
+                }
+                return item;
+            }
+        ))
+    }
+
+    function buttonHandler() {
+        setDicesRandomNumber(prev => prev.map((item)=>{
+            if (item.isHeld === false){
+                return {...item,value:getaRandomDice()}
+            }
+            return item;
+        }))
+    }
+
+    function startNewGame() {
+        setDicesRandomNumber(generateDiceRolls())
+        buttonText.current.textContent = "Roll the Dice"
+        setNewGame(false);
+    }
+
     return(
         <main>
             <div className={styles.dieWrapper}>
                     {dicesRandomNumber.map((dice)=>(
-                        <Die number={dice}/>
+                        <Die    key={dice.id} 
+                                dice={dice}
+                                setFunction={changeOnHeld}/>
                     ))}
+            </div>
+            <button 
+                ref={buttonText}
+                onClick={newGame ? ()=>startNewGame() : ()=>buttonHandler()}>
+                Roll the Dice
+            </button>
+        </main>
+    )
+}
+
+
+{/* {showDieMap} */}
                 {/* <Die number={1}/>
                 <Die number={2}/>
                 <Die number={3}/>
@@ -28,7 +95,3 @@ export default function MainComp() {
                 <Die number={2}/>
                 <Die number={6}/>
                 <Die number={6}/> */}
-            </div>
-        </main>
-    )
-}
