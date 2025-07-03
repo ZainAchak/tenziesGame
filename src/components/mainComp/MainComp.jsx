a02455aa23c16978fb70431322cb8cfdeab4140a
Zain Ullah <zainullahachakzai@gmail.com>
2025-07-03 19:50:54 +0500

    adding react confetti

--- src/components/mainComp/MainComp.jsx
+++ src/components/mainComp/MainComp.jsx
@@ -1,6 +1,8 @@
 import {useState } from "react";
 import Die from "../Die/Die";
 import styles from "./MainComp.module.css"
+import Confetti from 'react-confetti'
+import { useWindowSize } from 'react-use'
 
 function generateDiceRolls() {
   const diceRolls = [];
@@ -19,11 +21,10 @@ function getaRandomDice() {
 
 
 export default function MainComp() {
-
     const [dicesRandomNumber,setDicesRandomNumber] = useState(generateDiceRolls());
     // const [newGame, setNewGame] = useState(false);
     let buttonText = "Roll the Dice"
-
+    const { width, height } = useWindowSize()
     const allHeld = dicesRandomNumber.every(die => die.isHeld === true)
     const firstValue = dicesRandomNumber[0]?.value
     const allSameValue = allHeld && dicesRandomNumber.every(die => die.value === firstValue)
@@ -31,7 +32,7 @@ export default function MainComp() {
 
     // if(allHeld) setNewGame(true)
     buttonText = allHeld ? (allSameValue ? "New Game" : "Wrong Sequence") : "Roll the Dice"
-
+    if(allSameValue) console.log("Game won")
     // useEffect(()=>{
     //     const allHeld = dicesRandomNumber.every(die => die.isHeld === true);
     //     if(allHeld){
@@ -69,11 +70,11 @@ export default function MainComp() {
 
     function startNewGame() {
         setDicesRandomNumber(generateDiceRolls())
-        buttonText.current.textContent = "Roll the Dice"
     }
 
     return(
         <main>
+            {allSameValue && <Confetti width={width} height={height}/>}
             <h1 className={styles.title}>Tenzies</h1>
             <p className={styles.instructions}>Roll until all dice are same. Click each die to freeze it at its current value between rolls.</p>
             <div className={styles.dieWrapper}>
